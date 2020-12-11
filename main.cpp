24bc1ba723fdb6a32dbeabf9ecfcf83fcd13816c
borisKopeykin <73913483+borisKopeykin@users.noreply.github.com>
2020-12-11 09:00:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,270 +1,174 @@
-//
-// This is example code from Chapter 6.7 "Trying the second version" of
-// "Software - Principles and Practice using C++" by Bjarne Stroustrup
-//
+#include <iostream>
+#include "std_lib.h"
+//#include "vector1.h"
+//#include "vector2.h"
+//#include "vector3.h"
 
-/*
-  This file is known as calculator02buggy.cpp
+template<typename T, typename A = std::allocator<T>> class vector3
+{
+    int sz; // Размер
 
-  I have inserted 5 errors that should cause this not to compile
-  I have inserted 3 logic errors that should cause the program to give wrong results
+    A all;
 
-  First try to find and remove the bugs without looking in the book.
-  If that gets tedious, compare the code to that in the book (or posted source code)
+    T* elem; // Указатель на элементы (или 0)
 
-  Happy hunting!
+    int space; // Количество элементов плюс количество свободных слотов
 
-*/
+public:
+    vector3() : sz{0}, elem{nullptr}, space{0} { }
 
-#include "std_lib_facilities.h"
-#include "func.h"
-
-//const char number = '8';
-//const char quit = 'q';
-//const char print = ';';
-//const string prompt = "> ";
-//const string result = "= ";
-
-//class Token
-//{
-//public:
-//  char kind;        /// what kind of token
-//  double value;     /// for numbers: a value
-
-//  Token (char ch)
-//    : kind{ ch }, value{ 0 }  { }
-
-//  Token (char ch, double val)
-//    : kind{ ch }, value{ val }  { }
-//};
-
-
-//class Token_stream
-//{
-//public:
-//  Token_stream ();    /// make a Token_stream that reads from cin
-//  Token get ();
-//  void putback(Token t);
-//  void ignore(char c);
-
-//private:
-//  bool full {false};  /// is there a Token in the buffer?
-//  Token buffer;        /// here we keep a Token put back using putback()
-//};
-
-
-//Token_stream::Token_stream ()
-//  : buffer{ 0 }        /// no Token in buffer
-//{
-//}
-
-//void Token_stream::ignore(char c)
-//{
-//    if (full && c==buffer.kind){
-//        full = false;
-//        return;
-//    }
-//    full = false;
-//    char ch = 0;
-//    while (cin>>ch)
-//        if (ch==c) return;
-//}
-
-//void Token_stream::putback (Token t)
-//{
-//  if (full)
-//    error("putback() into a full buffer");
-
-//  buffer = t;
-//  full = true;
-//}
-
-
-//Token Token_stream::get ()
-//{
-//  if (full)         // do we already have a Token ready?
-//  {
-//    full = false;   // remove Token from buffer
-//    return buffer;
-//  }
-
-//  char ch;
-//  cin >> ch;  // note that >> skips whitespace (space, newline, tab, ...)
-
-//  switch (ch)
-//  {
-//  case print:    // for "print"
-//  case quit:    // for "quit"
-//  case '(':
-//  case ')':
-//  case '+':
-//  case '-':
-//  case '*':
-//  case '/':
-//  case '%':
-//    return Token{ ch };      // let each character represent itself
-//  case '.':
-
-//  case '0': case '1': case '2': case '3': case '4':
-//  case '5': case '6': case '7': case '8': case '9':
-//  {
-//    cin.putback(ch);         // put digit back into the input stream
-//    double val;
-//    cin >> val;              // read a floating-point number
-//    return Token{number, val};  // let '8' represent "a number"
-//  }
-
-//  default:
-//    error("Bad token");
-//  }
-//}
-
-
-
-//Token_stream ts;
-
-//double expression ();
-
-
-///// deal with numbers and parentheses
-//double primary ()
-//{
-//  Token t = ts.get();
-//  switch (t.kind)
-//  {
-//  case '(':          // handle '(' expression ')'
-//  {
-//    double d = expression();
-//    t = ts.get();
-//    if (t.kind != ')')
-//      error("')' expected");
-//    return d;
-//  }
-
-//  case number:          // we use '8' to represent a number
-//    return t.value;
-
-//  case '-':
-//      return - primary();
-
-//  case '+':
-//      return primary();
-
-//  default:
-//    error("primary expected");
-//  }
-//}
-
-
-///// deal with *, /, and %
-//double term ()
-//{
-//  double left = primary();
-//  Token t = ts.get();    // get the next token from token stream
-
-//  while (true)
-//  {
-//    switch (t.kind)
-//    {
-//    case '*':
-//      left *= primary();
-//      t = ts.get();
-//      break;
-
-//    case '%':
-//    {
-//      double d = primary();
-//      if (d == 0) error("divide by zero");
-//      left = fmod(left,d);
-//      t = ts.get();
-//      break;
-//    }
-
-//    case '/':
-//    {
-//      double d = primary();
-//      if (d == 0) error("divide by zero");
-//      left /= d;
-//      t = ts.get();
-//      break;
-//    }
-
-//    default:
-//      ts.putback(t);     // put t back into the token stream
-//      return left;
-//    }
-//  }
-//}
-
-
-///// deal with + and -
-//double expression ()
-//{
-//  double left = term();  // read and evaluate a Term
-//  Token t = ts.get();    // get the next token from token stream
-
-//  while (true)
-//  {
-//    switch (t.kind)
-//    {
-//    case '+':
-//      left += term();    // evaluate Term and add
-//      t = ts.get();
-//      break;
-
-//    case '-':
-//      left -= term();    // evaluate Term and subtract
-//      t = ts.get();
-//      break;
-
-//    default:
-//      ts.putback(t);     // put t back into the token stream
-//      return left;       // finally: no more + or -: return the answer
-//    }
-//  }
-//}
-
-//void clean_up_mess()
-//{
-//    ts.ignore(print);
-//}
-
-//void calculate()
-//{
-//    while (cin)
-//    try {
-//      cout << prompt;
-//      Token t = ts.get();
-//      while (t.kind == print) t = ts.get();  // 'q' for quit
-//      if (t.kind == quit) return;       // ';' for "print now"
-//      ts.putback(t);
-//      cout << result << expression() << '\n';
-//     }
-//    catch (exception& e){
-//        cerr << e.what() << '\n';
-//        clean_up_mess();
-//    }
-//}
-
-int main () {
-    try{
-        cout << "Welcome to calculator-programme!" << endl;
-        cout << "'q' - quit, ';' - output the result" << endl;
-        calculate();
-        keep_window_open();
-        return 0;
-    }
-    catch (runtime_error& e)
+    T& at(int n)
     {
-        cerr <<"error: "<< e.what() <<'\n';
-        keep_window_open("~~");
-        return 1;
+            if (n < 0 || sz <= n) throw out_of_range();
+            return elem[n];
     }
-    catch (...)
+
+    const T& at(int n) const
     {
-        cerr <<"Oops: unknown exception!\n";
-        keep_window_open("~~");
-        return 2;
+            if (n < 0 || sz <= n) throw out_of_range();
+            return elem[n];
     }
+
+    explicit vector3(int s):sz{s}, elem{new T[s]}, space{s}
+    {
+        for (int i = 0; i < sz; ++i)  // Элементы
+            elem[i] = 0;              // инициализированы
+    }
+
+    vector3(std::initializer_list<T> lst):sz{lst.size()}, elem{new T[sz]}, space{lst.size()}{
+               std::copy(lst.begin(), lst.end(), elem);
+           }
+
+    vector3(const vector3& copy_vector):sz{copy_vector.size()}, elem{new T[sz]}, space{copy_vector.capacity()} {
+               std::copy(copy_vector.elements, copy_vector.elements + sz, elem);
+           }
+
+    // Копирующие конструктор и присваивание:
+//    vector3(const vector3&);
+//    vector3& operator=(const vector3&);
+
+    // Перемещающие конструктор и присваивание:
+//    vector3(vector3&&);
+//    vector3& operator=(vector3&&);
+
+    ~vector3() { delete[] elem; } // Деструктор
+
+    T& operator[] (int n) {return elem[n];} // Доступ для неконстантного вектора
+    T operator[] (int n) const {return elem[n];} // Доступ для
+                                                             // константного вектора
+    int size() const { return sz; } // Возвращает значение размера вектора
+
+//    void reserve(int newallock); // Добавление памяти для новых элементов
+
+    int capacity() const { return space; } // Возвращает размер доступной памяти
+
+//    void resize(int newsize); // Устанавливает размер вектора
+
+//    void push_back(double d); // Добавление элемента в конец вектора
+
+
+
+T *p = new T[copy_vector.size()]
+{
+        for (int i=0; i < copy_vector.size(); i++)
+            p[i] = copy_vector.elem[i];
+        delete[] elem;
+        elem = p;
+        space = sz = copy_vector.size();
+        return *this;
+}
+
+vector3(vector3&& a):sz{a.size()}, elem{ a.elem}, space{a.capacity()}
+{
+        a.sz = 0;
+        a.space = 0;
+        a.elem = nullptr;
+}
+
+// Перемещающий конструктор:
+vector3(vector3&& a)
+    :sz{a.sz}, elem{a.elem}
+{
+    a.sz = 0;
+    a.elem = nullptr;
+}
+
+
+// Копирующее присваивание:
+vector3& operator=(const vector3& copy_vector)
+{
+    if (this == &copy_vector) return *this;
+
+    if (copy_vector.size() <= space){
+        for (int i(0); i < copy_vector.size(); i++)
+            elem[i] = copy_vector.elements[i];
+        sz = copy_vector.size();
+        return *this;
+}
+
+
+// Перемещающее присваивание:
+vector3& operator=(vector3&& a)
+{
+    delete[] elem;
+    elem = a.elem;
+    sz = a.sz();
+    space = a.capacity();
+    a.elem = nullptr;
+    a.sz = 0;
+    a.space = 0;
+    return *this;
+}
+
+
+// Добавление памяти для записи новых элементов:
+void reserve(int newallock)
+{
+    if (newallock <= space) return;
+    T* p = all.allocate(newallock);
+    for (int i=0; i<sz; ++i)
+        all.construct(&p[i], elem[i]);
+    for (int i=0; i<sz; ++i)
+        all.destroy(&elem[i]);
+    all.deallocate(elem, space);
+    elem = p;
+    space = newallock;
+}
+
+
+// Установка размера вектора:
+void resize(int newsize, T value = T())
+{
+    reserve(newsize);
+    for (int i=sz; i<newsize; ++i)
+        all.construct(&elem[i], value);
+    for (int i=sz; i<newsize; ++i)
+        all.destroy(&elem[i]);
+    sz = newsize;
+}
+
+
+// Добавление элемента в конец вектора:
+void push_back(const T& value)
+{
+    if (space==0)
+        reserve(8);
+    else if (sz==space)
+        reserve (2*space);
+    all.construct(&elem[sz], value);
+    ++sz;
+}
+
+};
+
+
+int main()
+{
+    vector3 v2(12);
+    v2.reserve(15);
+    v2[0] = 10;
+    std::cout << v2[10] << std::endl;
+    std::cout << v2.capacity() - v2.size();
+    return 0;
 }
 
